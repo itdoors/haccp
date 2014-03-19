51d2617264e79b92d0ed798beb6f0ec30f20b099
Pavel <ppecheny@i.ua>
2014-03-19 12:39:41 +0200

    api v1 point get

--- src/ITDoors/HaccpBundle/Services/PointApiV1Service.php
+++ src/ITDoors/HaccpBundle/Services/PointApiV1Service.php
@@ -44,9 +44,50 @@ class PointApiV1Service
 
         $points = $pr->ApiV1Get($ids);
 
-        foreach ($points as &$point)
+        foreach ($points as $key => &$point)
         {
             $statistics = $this->pointStatisticsApiV1Service->getLastStatistics($point['id']);
+
+            $point['plan'] = array();
+            $point['plan']['id'] = $point['planId'];
+            $point['plan']['name'] = $point['planName'];
+
+            $point['group'] = array();
+            $point['group']['id'] = $point['groupId'];
+            $point['group']['name'] = $point['groupName'];
+
+            $point['contour'] = array();
+            $point['contour']['id'] = $point['contourId'];
+            $point['contour']['name'] = $point['contourName'];
+
+            unset(
+                $points[$key]['planId'],
+                $points[$key]['planName'],
+                $points[$key]['groupId'],
+                $points[$key]['groupName'],
+                $points[$key]['contourId'],
+                $points[$key]['contourName']
+            );
+
+
+            foreach ($statistics as $key => &$value)
+            {
+                $value['characteristic'] = array();
+
+                $value['characteristic']['id'] = $value['characteristicId'];
+                $value['characteristic']['name'] = $value['characteristicName'];
+                $value['characteristic']['unit'] = $value['characteristicUnit'];
+                $value['characteristic']['criticalValueBottom'] = $value['criticalValueBottom'];
+                $value['characteristic']['criticalValueTop'] = $value['criticalValueTop'];
+
+                unset(
+                    $value['characteristicId'],
+                    $value['characteristicName'],
+                    $value['characteristicUnit'],
+                    $value['criticalValueBottom'],
+                    $value['criticalValueTop']
+                );
+            }
 
             $point['statistics'] = $statistics;
         }
