8e9641aa2512305012bfb56014f6a7a4110b67b8
Pavel <ppecheny@i.ua>
2014-03-27 18:50:16 +0200

    log

---
+++ src/ITDoors/HaccpBundle/Services/LogService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ITDoors\HaccpBundle\Services;
+use Symfony\Component\DependencyInjection\Container;
+
+/**
+ * Log service
+ */
+class LogService
+{
+    /**
+     * @var Container $container
+     */
+    protected $container;
+
+    /**
+     * __construct
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Write content to log file
+     *
+     * @param mixed $content
+     */
+    public function write($content)
+    {
+        $logFile = $this->container->getParameter('log.file.path');
+        $f1 = fopen($logFile, "a");
+        fwrite($f1, $content);
+        $output = PHP_EOL;
+        fwrite($f1, $output);
+        fclose($f1);
+    }
+}
